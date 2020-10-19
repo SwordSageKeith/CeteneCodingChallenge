944821dc94d2182e4ba7ee95e5af88380d51d033
Keith <visually.basic.keith@gmail.com>
2020-10-19 11:41:47 -0400

    oops forgot to upload to github

---
+++ src/main/java/com/cognixia/cetene/demo/CeteneCodingChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.cetene.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeteneCodingChallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeteneCodingChallangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognixia/cetene/demo/CeteneCodingChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognixia.cetene.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeteneCodingChallangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
